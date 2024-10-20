2948cffde5c5792f351702ca27b2571b1d5a3c81
thomasherstad <github@theormsats.fea.st>
2024-10-20 13:35:59 +0200

    Added broken tests

---
+++ internal/auth/aut_test.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	expected := "test-api-key"
+	testHeader := http.Header{}
+	testHeader.Add("Authorization", "ApiKe "+expected)
+
+	result, err := GetAPIKey(testHeader)
+	if err != nil {
+		t.Errorf("Test failed: Unexpected error: %v", err)
+		return
+	}
+
+	if result != expected {
+		t.Errorf("Test failed: expected %s, got %s", expected, result)
+	}
+}
